3b258f8cf42710ac64cd4031bba40b3b0dd5d893
Advait Patil <156990168+02advait@users.noreply.github.com>
2024-04-28 18:38:43 +0530

    I_can_make_search_on_google,youtube&wiki

--- JARVIS.py
+++ JARVIS.py
@@ -4,26 +4,37 @@ import datetime
 import wikipedia
 import webbrowser
 import os
+import pywhatkit
+#import smtplib
+#import openai
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 #print(voices[0].id)
 engine.setProperty('voice',voices[0].id)
 
-'''it speecks'''
 
+
+
+
+
+'''it speecks'''
+'''takes audio input from microphone'''
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-def takeCommand():
-    ''' it takes microphone input from user and returns string as output'''   
+
+
+''' it takes microphone input from user and returns string as output'''
+'''it can give errors so we use try, except and return'''
+def takeCommand():       
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
-
     try:
         print('Recognizing...')
         query = r.recognize_google(audio, language='en-in')
@@ -34,6 +45,9 @@ def takeCommand():
     return query
 
 
+
+
+'''gives the current date and time, and wishes me.:D '''
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
@@ -46,15 +60,36 @@ def wishMe():
         speak("Good Evening!")
         print("Good Evening!")
 
-speak("I am Jarvis Sir. Please tell me how may i help you")
 
+
+def searchGoogle(query):
+    if "google" in query:
+        import wikipedia as googleScrap
+        query = query.replace("jarvis","")
+        query = query.replace("google search","")
+        query = query.replace("google","")
+        speak("This is what I found on google")
+
+        try:
+            pywhatkit.search(query)
+            result = googleScrap.summary(query,1)
+            speak(result)
+
+        except:
+            speak("No speakable output available")
+
+
+
+
+
+speak("Hello I an  Jarvis A.I..  how may i help you Sir")
 
 if __name__ == "__main__":
     wishMe()
     while True :
         query = takeCommand().lower()
         # logic for executing task based on querry
-        if 'wikipedia' in query:
+        if 'xyz' in query:
             speak('Searching in Wikipedia...wait a while :)')
             query = query.replace('wikipedia',"")
             results = wikipedia.summary(query, sentences = 2)
@@ -75,15 +110,11 @@ if __name__ == "__main__":
         elif 'time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M")
             speak(f"Sit, the time is {strTime}")
-        elif 'vs code ' in query:
-            vscodePath = ' C:\\Users\\Advait\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe '
+        elif 'vs code' in query:
+            vscodePath = "C:\\Users\\Advait\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(vscodePath)
-        elif 'wallpaper' in query:
-            wallpaperPath = ' "C:\\Users\\Advait\\AppData\\Local\\Programs\\Lively Wallpaper\\Lively.exe" '
-            os.startfile(wallpaperPath)
-
-
-
-
-
-
+#        elif 'wallpaper' in query:
+#            wallpaperPath = "C:\\Users\\Advait\\AppData\\Local\\Programs\\Lively Wallpaper\\Lively.exe"
+#            os.startfile(wallpaperPath)
+        elif "google" in query:
+            searchGoogle(query)
